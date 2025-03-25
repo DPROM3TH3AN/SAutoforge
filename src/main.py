2ee2b8fba7d2100ee022542264ff08866134eec0
DPROM3TH3AN <muballigh888@gmail.com>
2025-03-25 14:07:47 -0400

    update

--- src/main.py
+++ src/main.py
@@ -9,9 +9,15 @@ app = FastAPI()
 def read_root():
     return {"message": "welcome to Autoforge"}
 
+
+
+"""
 from api import router as api_router
 app.include_router(api_router)
 
-if __name__ == "__main__":
+
+"if __name__ == "__main__":
    port = int(os.environ.get("PORT", 8000))
    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
+
+"""
