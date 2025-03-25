44c1665e5792ebd99279379a9b2f0436008d8c87
DPROM3TH3AN <muballigh888@gmail.com>
2025-03-25 13:22:35 -0400

    update

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "welcome to autoforge"}
+    return {"message": "welcome to Autoforge"}
 
 from api import router as api_router
 app.include_router(api_router)
